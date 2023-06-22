c5fb4166a37cdf42ac38735668412f82f8bf71fa
showyha08 <over.and.uver@gmail.com>
2023-06-22 21:53:56 +0900

    add_method_to_menuListFragment

--- app/src/main/java/com/websarva/wings/android/fragmentsample1/MenuListFragment.kt
+++ app/src/main/java/com/websarva/wings/android/fragmentsample1/MenuListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,7 +24,37 @@ class MenuListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_list, container, false)
+        // フラグメントで表示する画面をXMLファイルからインフレートする
+        val view = inflater.inflate(R.layout.fragment_menu_list, container, false)
+        // 画面部品ListViewを取得
+        val lvMenu = view.findViewById<ListView>(R.id.lvMenu)
+        // SimpleAdapterで使用するMutableListオブジェクトを用意
+        val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
+        // 「唐揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録
+        var menu = mutableMapOf("name" to "唐揚げ定食", "price" to "850円")
+        menuList.add(menu)
+        // 「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "850円")
+        menuList.add(menu)
+        // 「生姜焼き」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録
+        menu = mutableMapOf("name" to "生姜焼き", "price" to "850円")
+        menuList.add(menu)
+        // SimpleAdapter 第4引き数from用データの用意
+        val from = arrayOf("name", "price")
+        // SimpleAdapter 第5引数 to 用データの用意
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        // SimpleAdapterを生成
+        val adapter = SimpleAdapter(
+            activity,
+            menuList,
+            android.R.layout.simple_list_item_2,
+            from,
+            to
+        )
+        // アダプタの登録
+        lvMenu.adapter = adapter
+
+        // インフレートされた画面を戻り地として返す
+        return view
     }
 }
