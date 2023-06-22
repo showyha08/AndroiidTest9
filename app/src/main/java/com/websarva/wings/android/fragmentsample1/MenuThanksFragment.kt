9092c6bf6e38663a4dae025f8968404be53779b0
showyha08 <over.and.uver@gmail.com>
2023-06-22 23:17:38 +0900

    mod_menuThanksFragment

--- app/src/main/java/com/websarva/wings/android/fragmentsample1/MenuThanksFragment.kt
+++ app/src/main/java/com/websarva/wings/android/fragmentsample1/MenuThanksFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +19,41 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuThanksFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_thanks, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_menu_thanks, container, false)
+        // 所属アクティビティからインテントを取得
+        val intent = activity?.intent
+        // インテントから引き継ぎデータをまとめたもの(Bundleオブジェクト)を取得
+        val extras = intent?.extras
+        // 定食と金額を取得
+        val menuName = extras?.getString("menuName")
+        val menuPrice = extras?.getString("menuPrice")
+        // 定食名と金額を表示させるTextViewを取得
+        val tvMenuName = view.findViewById<TextView>(R.id.tvMenuName)
+        val tvMenuPrice = view.findViewById<TextView>(R.id.tvMenuPrice)
+        // TextViewに定食名と金額を表示
+        tvMenuName.text = menuName
+        tvMenuPrice.text = menuPrice
+        //戻るボタンを取得
+        val btBackButton = view.findViewById<Button>(R.id.btBackButton)
+        //戻るボタンにリスナ登録
+        btBackButton.setOnClickListener(ButtonClickListener())
+        //
+        return view
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuThanksFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuThanksFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    // ボタンが押されたときの処理が記述されたメンバクラス
+    private inner class ButtonClickListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            //自分が所属するアクティビティを終了
+            activity?.finish()
+        }
     }
+
 }
